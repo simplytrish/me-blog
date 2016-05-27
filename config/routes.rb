f274cbb6dd689ab14f7d975610cd961585e62c46
simplytrish <tracey@says.com>
2016-05-27 18:08:23 +0800

    Added devise and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root "posts#index"
+  get '/about', to: 'pages#about'
+
+  
   resources :posts do
     resources :comments
   end
   
-  root "posts#index"
 
-  get '/about', to: 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
