3498287ad4dc6ff538b386ebb31bae6cb7651fec
tracey <traceynorkjaer@yahoo.com>
2017-01-24 18:25:17 +0900

    Adding production database tables

--- db/schema.rb
+++ db/schema.rb
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 20170120151800) do
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "body",        limit: 65535
+    t.string   "cover",       limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.string   "cover",       limit: 255
     t.text     "profile",     limit: 65535
     t.text     "description", limit: 65535
   end
