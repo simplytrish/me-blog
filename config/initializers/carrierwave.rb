b98451e9ed691b98bca678e8bccd319155068fb7
tracey <traceynorkjaer@yahoo.com>
2017-06-14 13:43:55 +0900

    changed names of s3 keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 
   CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => "AWS",                        # required
-    :aws_access_key_id      => "AWS_KEY_ID",                        # required
-    :aws_secret_access_key  => "AWS_KEY",
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
     :region                 => "ap-northeast-1"       # required
   }
-  config.fog_directory  = ENV["S3_BUCKET"]                     # required
+  config.fog_directory  = ENV['S3_BUCKET']                     # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
